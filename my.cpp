ff047e6f01882b695f57607c2de2bde888b1e1ab
IsLeah00 <42385465+IsLeah00@users.noreply.github.com>
2020-11-24 20:48:02 +0100

    Update my.cpp

--- my.cpp
+++ my.cpp
@@ -1,12 +1,13 @@
-#include "std_lib_facilities.h"
 #include "my.h"
+#include "std_lib_facilities.h"
 
-int foo;
 void print_foo()
 {
-    cout << foo << '\n';
+    cout << foo << endl;
 }
+
+
 void print(int i)
 {
-    cout << i << '\n';
+    cout << i << endl;
 }
