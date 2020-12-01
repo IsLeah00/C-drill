00aa6b938fb58344c9f4a5695c872eb1244929d0
IsLeah00 <42385465+IsLeah00@users.noreply.github.com>
2020-12-01 22:39:15 +0100

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -8,7 +8,7 @@ void swap_v(int a, int b)
     b = temp;
 }
 
-void swap_r(int a, int b)
+void swap_r(int& a, int& b)
 {
     int temp;
     temp = a;
@@ -16,21 +16,36 @@ void swap_r(int a, int b)
     b = temp;
 }
 
+void swap_cr(const int& a, const int& b)
+{
+    int temp;
+    int temp2;
+    temp = a;
+    temp2 = b;
+
+    swap_r(temp, temp2);
+}
 
 int main()
 {
     int x = 7;
     int y = 9;
+    
     swap_r(x, y);
-    swap_r(7, 9);
+    swap_cr(7, 9);
+    
     const int cx = 7;
     const int cy = 9;
-    swap_r(cx, cy);
-    swap_r(7.7, 9.9);
+    
+    swap_v(cx, cy);
+    swap_cr(7.7, 9.9);
+    
     double dx = 7.7;
     double dy = 9.9;
-    swap_r(dx, dy);
-    swap_r(7.7, 9.9);
+    
+    swap_v(dx, dy);
+    swap_cr(7.7, 9.9);
+
 
     cout << "x: " << x << '\n'
          << "y: " << y << '\n'
